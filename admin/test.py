921754f6d27a776bd911440184e9590aa1c85af8
tem12qaz <tem12qaz@yandex.ru>
2022-03-04 18:41:00 +0300

    edit requests

--- admin/test.py
+++ admin/test.py
@@ -9,29 +9,40 @@ fa = UserAgent()
 
 
 cookies = {
-    'JSESSIONID': '513E12323FCFF9C7C18CEC67B492F73D',
-    'ks.tck': '98903ec27e7c0d28095031cf26d9c831',
-    'ks.ngs.oc': 'd1ab8e5a46259e3fdec85eab4a902f70',
+    'JSESSIONID': 'FC8EED28B4520200C93B970739C0AECA',
+    'ks.tck': '6e6761e26e9fa8cf83846764f0d27794',
     'k_stat': '0bd3355e-c17f-4431-885c-25769b5ce49f',
     'ks.tg': '25',
     '_ALGOLIA': 'anonymous-95f49eba-b37d-4386-a0f6-e3b0325f26b9',
     'ssaid': '0b826d00-9b62-11ec-9e8d-b90c9f2db05a',
     '_ga': 'GA1.2.1389746463.1646360595',
     '_gid': 'GA1.2.2055645266.1646360595',
-    '_hjFirstSeen': '1',
-    '_hjSession_283363': 'eyJpZCI6ImQ5YWVjNDcyLTgwYTAtNGIzNi1hMzVjLTE5YWYyOWFhMjg5NSIsImNyZWF0ZWQiOjE2NDYzNjA1OTQ1NzMsImluU2FtcGxlIjpmYWxzZX0=',
-    '_hjIncludedInSessionSample': '0',
-    '_hjAbsoluteSessionInProgress': '0',
-    '_gat_ddl': '1',
     '_ym_uid': '1646360595443082129',
     '_ym_d': '1646360595',
     '_ym_isad': '2',
     '_fbp': 'fb.1.1646360594905.720965103',
-    '_ym_visorc': 'b',
     'kaspi.storefront.cookie.city': '750000000',
     'ks.cc': '-1',
-    '__tld__': 'null',
     '_hjSessionUser_283363': 'eyJpZCI6Ijg5ODdhZDU3LTFjNGEtNTZmNS05MjNiLTU2YTk5ZDZkOTk3ZSIsImNyZWF0ZWQiOjE2NDYzNjA1OTQ1NjEsImV4aXN0aW5nIjp0cnVlfQ==',
+    'ks.ngs.s': '09e61e0041fa3f3ef4a84dcc680f7abb',
+    '_ym_visorc': 'b',
+    '_hjSession_283363': 'eyJpZCI6IjNkMTdlNWI4LTQwNDctNGIxMS1hY2U3LTY5MDA5NDU1N2U0ZCIsImNyZWF0ZWQiOjE2NDY0MDc0OTA3NDEsImluU2FtcGxlIjpmYWxzZX0=',
+    '_hjAbsoluteSessionInProgress': '0',
+    '_dyjsession': 'i449ioaynegsta0uuz3quzx5zhosflnx',
+    '_dy_csc_ses': 'i449ioaynegsta0uuz3quzx5zhosflnx',
+    '_dy_c_exps': '',
+    '_dycnst': 'dg',
+    '_dyid': '896217199371497286',
+    '_dy_df_geo': 'Russia..Moscow',
+    '_dy_geo': 'RU.EU.RU_MOW.RU_MOW_Moscow',
+    '_dy_toffset': '0',
+    '_dyid_server': '896217199371497286',
+    '_hjIncludedInSessionSample': '0',
+    '_dy_c_att_exps': '',
+    '_dycst': 'dk.w.c.ss.',
+    '_dy_soct': '1048286.1115542.1646407494.i449ioaynegsta0uuz3quzx5zhosflnx*1014383.1024804.1646408318*1019791.1035034.1646408318*1018356.1031862.1646408318',
+    '__tld__': 'null',
+    '_gali': 'page',
 }
 
 headers = {
@@ -63,6 +74,7 @@ proxies = {
 
 # proxies = {
 #     "http": "http://rYtq9n:hjdPab@46.232.5.3:8000/",
+#     "https": "http://rYtq9n:hjdPab@46.232.5.3:8000/",
 # }
 
 
@@ -105,4 +117,4 @@ def test():
 
 
 # print(parse_new(url))
-test()
+# test()
