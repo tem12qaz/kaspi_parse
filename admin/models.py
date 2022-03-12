cb9c53403d21996a2734b49de6d0bc1b3e921798
Matvey <tem12qaz@yandex.ru>
2022-03-13 01:05:20 +0300

    init

--- admin/models.py
+++ admin/models.py
@@ -24,17 +24,92 @@ class Role(db.Model, RoleMixin):
     description = db.Column(db.String(255))
 
 
+class Proxie(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+
+
+
 class Product(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     region = db.Column(db.String(128), nullable=True)
     name = db.Column(db.String(512), nullable=True)
     kaspi_price = db.Column(db.Integer(), nullable=True)
     kaspi_url = db.Column(db.String(512))
-    supplier1_code = db.Column(db.String(128))
+
+    supplier1_code = db.Column(db.String(128), nullable=True)
+    supplier1_amount = db.Column(db.String(128), nullable=True)
     supplier1_name = db.Column(db.String(128), nullable=True)
     supplier1_price = db.Column(db.Float(), nullable=True)
     supplier1_margin = db.Column(db.Float(), nullable=True)
     supplier1_margin_percent = db.Column(db.Float(), nullable=True)
+
+    supplier2_code = db.Column(db.String(128), nullable=True)
+    supplier2_amount = db.Column(db.String(128), nullable=True)
+    supplier2_name = db.Column(db.String(128), nullable=True)
+    supplier2_price = db.Column(db.Float(), nullable=True)
+    supplier2_margin = db.Column(db.Float(), nullable=True)
+    supplier2_margin_percent = db.Column(db.Float(), nullable=True)
+
+    supplier3_code = db.Column(db.String(128), nullable=True)
+    supplier3_amount = db.Column(db.String(128), nullable=True)
+    supplier3_name = db.Column(db.String(128), nullable=True)
+    supplier3_price = db.Column(db.Float(), nullable=True)
+    supplier3_margin = db.Column(db.Float(), nullable=True)
+    supplier3_margin_percent = db.Column(db.Float(), nullable=True)
+
+    supplier4_code = db.Column(db.String(128), nullable=True)
+    supplier4_amount = db.Column(db.String(128), nullable=True)
+    supplier4_name = db.Column(db.String(128), nullable=True)
+    supplier4_price = db.Column(db.Float(), nullable=True)
+    supplier4_margin = db.Column(db.Float(), nullable=True)
+    supplier4_margin_percent = db.Column(db.Float(), nullable=True)
+
+    supplier5_code = db.Column(db.String(128), nullable=True)
+    supplier5_amount = db.Column(db.String(128), nullable=True)
+    supplier5_name = db.Column(db.String(128), nullable=True)
+    supplier5_price = db.Column(db.Float(), nullable=True)
+    supplier5_margin = db.Column(db.Float(), nullable=True)
+    supplier5_margin_percent = db.Column(db.Float(), nullable=True)
+
+    supplier6_code = db.Column(db.String(128), nullable=True)
+    supplier6_amount = db.Column(db.String(128), nullable=True)
+    supplier6_name = db.Column(db.String(128), nullable=True)
+    supplier6_price = db.Column(db.Float(), nullable=True)
+    supplier6_margin = db.Column(db.Float(), nullable=True)
+    supplier6_margin_percent = db.Column(db.Float(), nullable=True)
+
+    supplier7_code = db.Column(db.String(128), nullable=True)
+    supplier7_amount = db.Column(db.String(128), nullable=True)
+    supplier7_name = db.Column(db.String(128), nullable=True)
+    supplier7_price = db.Column(db.Float(), nullable=True)
+    supplier7_margin = db.Column(db.Float(), nullable=True)
+    supplier7_margin_percent = db.Column(db.Float(), nullable=True)
+
+    supplier8_code = db.Column(db.String(128), nullable=True)
+    supplier8_amount = db.Column(db.String(128), nullable=True)
+    supplier8_name = db.Column(db.String(128), nullable=True)
+    supplier8_price = db.Column(db.Float(), nullable=True)
+    supplier8_margin = db.Column(db.Float(), nullable=True)
+    supplier8_margin_percent = db.Column(db.Float(), nullable=True)
+
+    supplier9_code = db.Column(db.String(128), nullable=True)
+    supplier9_amount = db.Column(db.String(128), nullable=True)
+    supplier9_name = db.Column(db.String(128), nullable=True)
+    supplier9_price = db.Column(db.Float(), nullable=True)
+    supplier9_margin = db.Column(db.Float(), nullable=True)
+    supplier9_margin_percent = db.Column(db.Float(), nullable=True)
+
+    supplier10_code = db.Column(db.String(128), nullable=True)
+    supplier10_amount = db.Column(db.String(128), nullable=True)
+    supplier10_name = db.Column(db.String(128), nullable=True)
+    supplier10_price = db.Column(db.Float(), nullable=True)
+    supplier10_margin = db.Column(db.Float(), nullable=True)
+    supplier10_margin_percent = db.Column(db.Float(), nullable=True)
+
+    best_name = db.Column(db.String(128), nullable=True)
+    best_margin = db.Column(db.Float(), nullable=True)
+    best_margin_percent = db.Column(db.Float(), nullable=True)
+
     commission_id = db.Column(db.Integer, db.ForeignKey('commission.id'), nullable=False)
 
     def __repr__(self):
@@ -45,6 +120,9 @@ s_price = {self.supplier1_price}
 s_margin = {self.supplier1_margin}
 s_margin_percent = {self.supplier1_margin_percent}
 '''
+
+    def parse_kaspi(self):
+
 
 
 class Commission(db.Model):
